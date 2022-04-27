7477e17721f797f5f6cd1788776a9f980790de6e
sanjasabovic <sanjasabovic@gmail.com>
2022-04-27 20:32:20 +0200

    Printing state.

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python3
 
 class Elevator:
-    pass
+    def __init__(self, min_floor=0, max_floor=5):
+        if max_floor <= min_floor:
+            raise Exception("Max_floor must be greater then min_floor.")
+        self.min_floor = min_floor
+        self.max_floor = max_floor
+        self.occupancy = False
+        self.direction = 0
+        self.curr_floor = 0
 
 
-def print_state(state):
-    pass
+def print_state(state: Elevator) -> None:
+    print(f"Current floor: {state.curr_floor}")
+    print(f"Direction: {state.direction}")
 
 
-def evaluate_changes(state, changes):
+def evaluate_changes(state, changes: str) -> Elevator:
     return state
 
 
-def read_changes():
-    return ()
+def read_changes() -> str:
+    line = input("> ")
+    return line
 
 
 if __name__ == "__main__":
