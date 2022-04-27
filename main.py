310235ba5d954099ae003e73ce4c3a96f4a8cc22
sanjasabovic <sanjasabovic@gmail.com>
2022-04-27 13:32:09 +0200

    Skeleton of the REPL

---
+++ main.py
@@ -0,0 +1,29 @@
+class Elevator:
+    pass
+
+
+def print_state(state):
+    pass
+
+
+def evaluate_changes(state, changes):
+    return state
+
+
+def read_changes():
+    return ()
+
+
+if __name__ == "__main__":
+    state = Elevator()
+    print_state(state)
+
+    # This is an:
+    while True:
+        # Read
+        changes = read_changes()
+        # Eval
+        state = evaluate_changes(state, changes)
+        # Print
+        print_state(state)
+        # Loop—a REPL!
