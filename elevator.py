92966a4b07d898cbf82be52e3e21b24a6492967e
sanjasabovic <sanjasabovic@gmail.com>
2022-04-29 21:42:36 +0200

    Added bool return for 3 main methods and type property which contains the name of the elevator

--- elevator.py
+++ elevator.py
@@ -2,13 +2,14 @@ class Elevator:
     def __init__(self, min_floor=0, max_floor=5):
         if max_floor <= min_floor:
             raise Exception("Top floor must be greater than the lowest one.")
+        self.type = None
         self.min_floor = min_floor
         self.max_floor = max_floor
         self.occupancy = False
         self.direction = 0
         self.curr_floor = 0
 
-    def all_floors(self):
+    def all_floors(self) -> range:
         return range(self.min_floor, self.max_floor + 1)
 
     def is_valid_floor(self, floor: int) -> bool:
@@ -36,33 +37,39 @@ class Elevator:
 class DumbElevator(Elevator):
     def __init__(self, min_floor=0, max_floor=5):
         super().__init__(min_floor, max_floor)
+        self.type = "Dumb elevator"
         self.destination_floor = 0
 
-    def display(self):
+    def display(self) -> str:
         available = self.direction == 0 and self.occupancy is False
         return f"Floor: {self.curr_floor}\nAvailable: {available}"
 
-    def next(self):
+    def next(self) -> bool:
         if self.destination_floor != self.curr_floor:
             self.curr_floor += self.direction
-        else:
-            self.direction = 0
+            return True
+        self.direction = 0
+        return False
 
-    def called_floor(self, floor_called: int):
+    def called_floor(self, floor_called: int) -> bool:
         if self.is_valid_floor(floor_called):
             if self.direction == 0 and self.occupancy is False and floor_called != self.curr_floor:
                 self.destination_floor = floor_called
                 self.direction = 1 if self.destination_floor > self.curr_floor else -1
-        else:
-            print(f"The {floor_called} floor doesn't exist in this building!")
+                return True
+            return False
+        print(f"The {floor_called} floor doesn't exist in this building!")
+        return False
 
-    def pressed_floor(self, floor_pressed: int):
+    def pressed_floor(self, floor_pressed: int) -> bool:
         if self.is_valid_floor(floor_pressed):
             if self.direction == 0 and self.occupancy is True and floor_pressed != self.curr_floor:
                 self.destination_floor = floor_pressed
                 self.direction = 1 if self.destination_floor > self.curr_floor else -1
-        else:
-            print(f"The {floor_pressed} floor doesn't exist in this building!")
+                return True
+            return False
+        print(f"The {floor_pressed} floor doesn't exist in this building!")
+        return False
 
     def people_in(self):
         if self.direction == 0:
@@ -76,6 +83,7 @@ class DumbElevator(Elevator):
 class SmartElevator(Elevator):
     def __init__(self, min_floor=0, max_floor=5):
         super().__init__(min_floor, max_floor)
+        self.type = "Smart elevator"
         self.direction = 0
         self.general_direction = 1
         self.floors_to_visit = [0]*(self.max_floor - self.min_floor)
