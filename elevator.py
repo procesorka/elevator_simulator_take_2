bd1eb98ce04a9c5ddde456f494c01680a7f086b4
sanjasabovic <sanjasabovic@gmail.com>
2022-04-29 16:49:39 +0200

    Class SmartElevator: methods. Not working as it should!

--- elevator.py
+++ elevator.py
@@ -76,9 +76,38 @@ class DumbElevator(Elevator):
 class SmartElevator(Elevator):
     def __init__(self, min_floor=0, max_floor=5):
         super().__init__(min_floor, max_floor)
+        self.direction = 0
+        self.general_direction = 1
+        self.floors_to_visit = [0]*(self.max_floor - self.min_floor)
 
     def display(self):
-        return f"Floor: {self.curr_floor}\nDirection: {self.direction}"
+        return f"Floor: {self.curr_floor}\nDirection: {self.direction}\n{self.floors_to_visit}"
+
+    def next(self):
+        if self.curr_floor == self.max_floor or self.curr_floor == self.min_floor:
+            self.direction *= -1
+
+        if self.floors_to_visit[self.curr_floor - self.min_floor] == 1:
+            self.floors_to_visit[self.curr_floor - self.min_floor] = 0
+        else:
+            self.curr_floor += self.direction
+
+    def called_floor(self, floor_called: int):
+        if self.is_valid_floor(floor_called):
+            if floor_called != self.curr_floor:
+                self.floors_to_visit[floor_called - self.min_floor] = 1
+        else:
+            print(f"The {floor_called} floor doesn't exist in this building!")
+
+    def pressed_floor(self, floor_pressed: int):
+        if self.is_valid_floor(floor_pressed):
+            if self.occupancy is True and floor_pressed != self.curr_floor:
+                self.floors_to_visit[floor_pressed - self.min_floor] = 1
+                # if any(self.floors_to_visit):
+
+        else:
+            print(f"The {floor_pressed} floor doesn't exist in this building!")
+
     def people_in(self):
         if self.direction == 0:
             self.occupancy = True
